dd9604d5b0619c61bd8e8abc8297fceff9933094
Isis Daron <daronisism@gmail.com>
2023-08-03 16:35:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Contador de Tags
+
+## 🚀 Sobre
+Esse projeto foi criado como solução para o teste prático. 
+
+### Descrição do problema
+Criar um programa para identificar as tags HTML existentes nas páginas que forem carregadas por meio de uma lista de URL informada. Além disso, deve ser contado quantas vezes cada tag aparece em cada página.
+
+É necessário mostrar as informações coletadas para possibilitar verificar os dados da URL informada. Assim, as URL, tags e as respectivas contagens devem ser armazenadas em um banco de dados.
+
+### Solução
+Para solucionar o problema proposto foram divididas as etapas por partes: 
+- Processar a URL inserida e contar as tags:
+
+Para processar a URL foi usada a requisição `fetch()` que retorna uma resposta, que depois é convertida para texto. Em seguida, foi utilizado o `DOMParser` para analisar o conteúdo HTML e logo após o método `querySelectorAll('*')` para selecionar todos os elementos. Através de um laço `forEach()` é contado quantas vezes cada tag HTML aparece no documento.
+
+- Armazenar os dados em um banco:
+
+A fim de realizar esse armazenamento foi escolhido o indexedDB como banco de dados, nele foram registradas a url inserida, os nomes das tags e suas respectivas contagens. Para cada url digitada, é criado um novo objeto `.objectStore('tags_html')` com as informações necessárias.
+
+> Esse banco pode ser consultado através da aba 'Aplicações' no Inspecionar Elemento do site. Nessa aba constará uma seção IndexedDB, clicando nela aparecerá o banco 'tags_html' que está armazenando todas as URLs digitadas. 
+
+
+- Consultar o banco de dados para visualizar as informações na tela:
+
+Com o objetivo de realizar essa consulta, foi utilizado a função `.openCursor(IDBKeyRange.only(URL))` que abre um cursor para percorrer todos os objetos do banco e encontrar o objeto que possui a exata URL informada. Quando é encontrado, os dados desse objetos são passados para uma função `inserirNaTabela()` que, através de um loop, cria linhas na tabela com os nomes e quantidades das tags da URL.
+
+#### Adicionais
+
+A fim de prevenir erros, a aplicação não aceita URLs inválidas e URLs cujo código possui um bloqueio de segurança e não pode ser acessado.
+
+Essa verificação foi feita através da declaração `try... catch`, onde no `catch()` será recebido qualquer erro ou exceção e retornará uma mensagem de erro. Para a validação de URLs utilizou-se uma função onde uma string é passada para o construtor `new URL()` que retornará uma URL caso a string seja válida.
+
+
+## 🔭 Pré-visualização
+
+<img src="src/images/img_tela.png" alt="Tela do Site">
+
+> URL do site: 
+
+## 👩🏻‍💻 Construído com:
+- HTML
+- CSS
+- JavaScript
---
+++ src/main.js
@@ -0,0 +1,140 @@
+document.getElementById("pegarUrl").addEventListener("click", processarUrl, false);
+
+// Função principal de processamento de URL, onde são chamadas as outras funções
+async function processarUrl() {
+    document.getElementById('tabela').classList.add('invisivel');
+    document.getElementById('erro').innerHTML = '';
+
+    try {
+        const url = await pegarUrl();
+        const conteudoHtml = await carregarConteudo(url);
+
+        const contagemTag = await contarTags(conteudoHtml);
+        await armazenarDados(url, contagemTag);
+        await consultarDados(url);
+    } catch (erro) {
+        document.getElementById('erro').innerHTML = 'Erro: Não foi possível acessar essa URL. Tente inserir outro link.';
+    }
+
+}
+
+// Função para pegar a URL digitada pelo usuário
+async function pegarUrl() {
+    const url = document.getElementById('urlInput').value.trim()
+    
+    if (!validandoUrl(url)) {
+        throw new Error('URL inválida: ' + url);
+    }
+
+    return url;
+}
+
+// Função para carregar o conteudo da URL
+async function carregarConteudo(url) {
+    const resposta = await fetch(url);
+
+    console.log(resposta)
+    return await resposta.text();
+}
+
+// Realizando a contagem das Tags da URL digitada
+async function contarTags(conteudoHtml) {
+    const contagemTag = {};
+
+    const parser = new DOMParser();
+    console.log(parser)
+    const doc = parser.parseFromString(conteudoHtml, 'text/html');
+    console.log(doc)
+
+    doc.querySelectorAll('*').forEach((elemento) => {
+        const nomeTag = elemento.tagName.toLowerCase();
+        contagemTag[nomeTag] = (contagemTag[nomeTag] || 0) + 1;
+    });
+    return contagemTag;
+}
+
+// Armazenando os dados (URL, Tags e quantidades) através da API de banco de dados IndexedDB
+async function armazenarDados(url, contagemTag) {
+    const request = indexedDB.open('banco_tags', 1);
+
+    request.onupgradeneeded = function (event) {
+        const bancoTags = event.target.result;
+        bancoTags.createObjectStore('tags_html', {
+            keyPath: 'url'
+        });
+    };
+
+    request.onsuccess = function (event) {
+        const bancoTags = event.target.result;
+        const transacao = bancoTags.transaction(['tags_html'], 'readwrite');
+        const objeto = transacao.objectStore('tags_html');
+
+        const arrayTags = Object.entries(contagemTag).map(([tag, quantidade]) => ({
+            tag,
+            quantidade
+        }));
+
+        objeto.put({
+            url,
+            tags: arrayTags
+        });
+    };
+}
+
+//Consultando e obtendo os dados armazenados no banco de dados
+async function consultarDados(url) {
+    const request = indexedDB.open('banco_tags', 1);
+
+    return new Promise((resolve) => {
+        request.onsuccess = function (event) {
+            const bancoTags = event.target.result;
+
+            const transacao = bancoTags.transaction(['tags_html'], 'readonly');
+            const objeto = transacao.objectStore('tags_html');
+
+            const getRequest = objeto.openCursor(IDBKeyRange.only(url));
+
+            getRequest.onsuccess = function (event) {
+                const cursor = event.target.result;
+                const dados = cursor.value;
+                inserirNaTabela(dados);
+                resolve(dados)
+            };
+        };
+    });
+}
+
+// Inserindo os dados na tabela
+function inserirNaTabela(dados) {
+    var tabela = document.getElementById('tabela');
+    var tbody = tabela.querySelector('tbody');
+
+    tbody.innerHTML = '';
+    console.log(dados)
+
+    dados.tags.forEach(function (tags) {
+        var tr = document.createElement('tr');
+        var tag = document.createElement('td');
+        var quantidade = document.createElement('td');
+
+        tag.textContent = tags.tag;
+        quantidade.textContent = tags.quantidade;
+
+        tr.appendChild(tag);
+        tr.appendChild(quantidade);
+        tbody.appendChild(tr);
+    });
+
+    tabela.className = tabela.className.replace('invisivel', '');
+
+}
+
+// Função para conferir se a URL digitada é valida
+function validandoUrl(url) {
+    try {
+        new URL(url);
+        return true;
+    } catch (erro) {
+        return false;
+    }
+}
